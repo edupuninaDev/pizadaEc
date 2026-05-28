88fc50e5db505472027727d92e548cc1b5b4d6db
edupuninaDev <epunina6299@uta.edu.ec>
2026-05-28 15:22:55 -0500

    fix: instalacion de swager para documentacion

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Pizada API')
+    .setDescription('API para la aplicación de Pizada')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   app.enableCors({
     origin: 'http://localhost:3000',
     credentials: true, // Reemplaza con el origen de tu frontend
